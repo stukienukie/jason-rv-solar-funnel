10cda3b591ae9f52517b978cf7f3ab8ebefb3807
stukienukie <luke@monox.ai>
2026-05-11 15:35:39 -0700

    Switch GTM container to GTM-KMTMCB29 (Jason's Funnel RV Solar)

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,13 +41,13 @@ export default function RootLayout({
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P24WZ22J');`}
+          })(window,document,'script','dataLayer','GTM-KMTMCB29');`}
         </Script>
       </head>
       <body className="font-sans antialiased">
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P24WZ22J"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KMTMCB29"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
